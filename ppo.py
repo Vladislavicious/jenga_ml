3dc6444f04bec2ee48746cec11a880cc3be9183f
vladislavicious <vladshilov03@mail.ru>
2025-12-02 21:10:08 +0300

    Начал делать агента

--- ppo.py
+++ ppo.py
@@ -1,6 +1,8 @@
+from typing import Tuple, List
+
 import torch
 import torch.nn as nn
-from typing import Tuple, List
+
 
 # Нейронная сеть для PPO
 class PPONetwork(nn.Module):
@@ -30,3 +32,31 @@ class PPONetwork(nn.Module):
         value = self.critic_head(features)
 
         return action_logits, value
+
+## constants to move them in future
+LEARNING_RATE: float = 3e-4
+GAE_LAMBDA: float = 0.95,
+GAMMA: float = 0.99,
+CLIP_EPSILON: float = 0.2,
+ENTROPY_COEF: float = 0.01,
+VALUE_COEF: float = 0.5,
+MAX_GRAD_NORM: float = 0.5,
+DEVICE: str = 'cuda'
+
+# Реализация алгоритма PPO
+class PPOAgent:
+    def __init__(
+        self,
+        state_dim: int,
+        action_dims: List[int],
+    ):
+        self.device = torch.device(DEVICE if torch.cuda.is_available() else 'cpu')
+        self.gamma = GAMMA
+        self.gae_lambda = GAE_LAMBDA
+        self.clip_epsilon = CLIP_EPSILON
+        self.entropy_coef = ENTROPY_COEF
+        self.value_coef = VALUE_COEF
+        self.max_grad_norm = MAX_GRAD_NORM
+
+        self.network: PPONetwork = PPONetwork(state_dim, action_dims).to(self.device)
+        self.optimizer = torch.optim.Adam(self.network.parameters(), lr=LEARNING_RATE)
