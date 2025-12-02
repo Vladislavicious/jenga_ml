5ad251f042917a112e9625cc6ccf95f479591fb7
vladislavicious <vladshilov03@mail.ru>
2025-12-02 22:44:36 +0300

    расширил API для environment

--- environment.py
+++ environment.py
@@ -1,7 +1,26 @@
 # there will be all MuJoCo-related stuff
 
 
-from typing import List
+from typing import List, Tuple
+
+import numpy as np
+
+
+class FakeRewardCalculator:
+    def __init__(self):
+        pass
+
+    # TODO: добавить параметры для настройки функции наград
+    # максимальное увеличение высоты
+    # количество упавших блоков
+    # стабильность башни
+    # кучность блоков
+    # максимальная скорость блоков
+    def fill_physics(self):
+        pass
+
+    def calculate_reward(self) -> float:
+        pass
 
 
 class FakeEnvironment:
@@ -17,4 +36,24 @@ class FakeEnvironment:
             11,
             11,
             11,
-        ]  # 10 blocks, 11 values each for power on X, Y, Z
+        ]  # 10 блоков, 11 значений каждой силы по каждой оси
+
+    # функция выполняет шаг симуляции, используя входные воздействия action
+    # по окончании action, заполняет параметрами reward_calculator, используя функцию fill_physics
+    # возвращает:
+    # next_state: np.ndarray - состояние окружения после выполнения шага
+    # simulation_end: bool - симуляция аварийно завершена/башня упала = True
+    def step(
+        self, action: np.ndarray, reward_calculator: FakeRewardCalculator
+    ) -> Tuple[np.ndarray, bool]:
+        pass
+
+    # cброс данных окружения (MuJoCo) - координаты каждого блока
+    # еще не уверен по поводу размеров блоков и нужны ли они алгоритму обучения
+    # возвращает нулевое состояние - после совершения сброса
+    def reset(self) -> np.ndarray:
+        pass
+
+    # вспомогательная функция для получения текущего состоянияы
+    def _get_current_state(self) -> np.ndarray:
+        return np.array()
--- model_trainer.py
+++ model_trainer.py
@@ -1,8 +1,7 @@
 from typing import Dict, List
 
 import numpy as np
-from torch import Tensor
-from environment import FakeEnvironment
+from environment import FakeEnvironment, FakeRewardCalculator
 from ppo import PPOAgent
 
 
@@ -30,6 +29,8 @@ class JengaML_Trainer:
             action_dims=action_dimensions,
         )
 
+        self.reward_calculator = FakeRewardCalculator()
+
     def train(self):
         """Основной цикл обучения"""
         print(f"Начало обучения на {self.total_timesteps} шагов")
@@ -37,19 +38,16 @@ class JengaML_Trainer:
         timestep = 0
 
         while timestep < self.total_timesteps:
-            # Сбор траектории
             step_data = self.perform_train_step()
             timestep += len(step_data["states"])
 
-            # Вычисление преимуществ
             advantages, returns = self.agent.compute_advantages(
                 rewards=step_data["rewards"],
                 values=step_data["values"],
                 dones=step_data["dones"],
-                next_value=step_data["last_value"],
+                next_value=step_data["last_action_value"],
             )
 
-            # Обновление политики
             policy_loss, value_loss, entropy = self.agent.update(
                 states=step_data["states"],
                 actions=step_data["actions"],
@@ -74,7 +72,38 @@ class JengaML_Trainer:
         values = []
         log_probs = []
 
-        last_value = Tensor()
+        state = self.model_environment.reset()
+        episode_reward = 0
+        episode_length = 0
+
+        for _ in range(self.n_steps):
+            action, log_prob, value = self.agent.get_action(state)
+
+            next_state, done = self.model_environment.step(
+                action, self.reward_calculator
+            )
+            reward = self.reward_calculator.calculate_reward()
+
+            states.append(state)
+            actions.append(action)
+            rewards.append(reward)
+            dones.append(done)
+            values.append(value)
+            log_probs.append(log_prob)
+
+            state = next_state
+            episode_reward += reward
+            episode_length += 1
+
+            if done:
+                self.episode_rewards.append(episode_reward)
+                self.episode_lengths.append(episode_length)
+
+                state = self.model_environment.reset()
+                episode_reward = 0
+                episode_length = 0
+
+        _, _, last_action_value = self.agent.get_action(state)
 
         step_data = {
             "states": np.array(states, dtype=np.float32),
@@ -83,7 +112,7 @@ class JengaML_Trainer:
             "dones": np.array(dones, dtype=bool),
             "values": np.array(values, dtype=np.float32),
             "log_probs": np.array(log_probs, dtype=np.float32),
-            "last_value": last_value,
+            "last_action_value": last_action_value,
         }
 
         return step_data
